218317542bee27b451f55f19c00cc04ae01f6060
Wojciech <wk8140p@greenwich.ac.uk>
2020-07-02 19:51:36 +0100

    loopsjs

---
+++ loops.js
@@ -0,0 +1,15 @@
+names = new Array ();
+i = 0;
+
+do {
+  next = window.prompt("Enter the next name", "");
+  if (next > " ") names [i] = next;
+  i = i + 1;
+} while (next > " ");
+
+document.write ("<h2>" + (names.length) + "names entered.</h2>");
+document.write ("<ol>");
+for (i in names) {
+  document.write ("<li>" + names [i] + "</li>");
+}
+document.write ("</ol>");
